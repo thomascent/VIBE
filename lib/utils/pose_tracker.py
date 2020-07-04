3119ccdc689bf37fd861a04c2e05fdf8351c5d88
Tom Stewart <thomas@ascent.ai>
2020-07-04 18:04:41 +0900

    using docker image for staf

--- lib/utils/pose_tracker.py
+++ lib/utils/pose_tracker.py
@@ -23,30 +23,43 @@ import os.path as osp
 
 
 def run_openpose(
-        video_file,
-        output_folder,
-        staf_folder,
-        vis=False,
+    video_file, output_folder, docker_image, vis=False,
 ):
-    pwd = os.getcwd()
-
-    os.chdir(staf_folder)
-
     render = 1 if vis else 0
     display = 2 if vis else 0
-    cmd = [
-        'build/examples/openpose/openpose.bin',
-        '--model_pose', 'BODY_21A',
-        '--tracking', '1',
-        '--render_pose', str(render),
-        '--video', video_file,
-        '--write_json', output_folder,
-        '--display', str(display)
+
+    docker_preamble = [
+        "docker",
+        "run",
+        "--gpus",
+        "all",
+        "--rm",
+        "-it",
+        "-v",
+        video_file + ":" + video_file,
+        "-v",
+        output_folder + ":" + output_folder,
+        docker_image,
     ]
 
-    print('Executing', ' '.join(cmd))
-    subprocess.call(cmd)
-    os.chdir(pwd)
+    cmd = [
+        "build/examples/openpose/openpose.bin",
+        "--model_pose",
+        "BODY_21A",
+        "--tracking",
+        "1",
+        "--render_pose",
+        str(render),
+        "--video",
+        video_file,
+        "--write_json",
+        output_folder,
+        "--display",
+        str(display),
+    ]
+
+    print("Executing", " ".join(docker_preamble + cmd))
+    subprocess.call(docker_preamble + cmd)
 
 
 def read_posetrack_keypoints(output_folder):
@@ -57,43 +70,39 @@ def read_posetrack_keypoints(output_folder):
         json_file = osp.join(output_folder, result_file)
         data = json.load(open(json_file))
         # print(idx, data)
-        for person in data['people']:
-            person_id = person['person_id'][0]
-            joints2d  = person['pose_keypoints_2d']
+        for person in data["people"]:
+            person_id = person["person_id"][0]
+            joints2d = person["pose_keypoints_2d"]
             if person_id in people.keys():
-                people[person_id]['joints2d'].append(joints2d)
-                people[person_id]['frames'].append(idx)
+                people[person_id]["joints2d"].append(joints2d)
+                people[person_id]["frames"].append(idx)
             else:
                 people[person_id] = {
-                    'joints2d': [],
-                    'frames': [],
+                    "joints2d": [],
+                    "frames": [],
                 }
-                people[person_id]['joints2d'].append(joints2d)
-                people[person_id]['frames'].append(idx)
+                people[person_id]["joints2d"].append(joints2d)
+                people[person_id]["frames"].append(idx)
 
     for k in people.keys():
-        people[k]['joints2d'] = np.array(people[k]['joints2d']).reshape((len(people[k]['joints2d']), -1, 3))
-        people[k]['frames'] = np.array(people[k]['frames'])
+        people[k]["joints2d"] = np.array(people[k]["joints2d"]).reshape(
+            (len(people[k]["joints2d"]), -1, 3)
+        )
+        people[k]["frames"] = np.array(people[k]["frames"])
 
     return people
 
 
-def run_posetracker(video_file, staf_folder, posetrack_output_folder='/tmp', display=False):
+def run_posetracker(video_file, staf_folder, posetrack_output_folder="/tmp", display=False):
     posetrack_output_folder = os.path.join(
-        posetrack_output_folder,
-        f'{os.path.basename(video_file)}_posetrack'
+        posetrack_output_folder, f"{os.path.basename(video_file)}_posetrack"
     )
 
     # run posetrack on video
-    run_openpose(
-        video_file,
-        posetrack_output_folder,
-        vis=display,
-        staf_folder=staf_folder
-    )
+    run_openpose(video_file, posetrack_output_folder, vis=display, staf_folder=staf_folder)
 
     people_dict = read_posetrack_keypoints(posetrack_output_folder)
 
-    shutil.rmtree(posetrack_output_folder)
+    # shutil.rmtree(posetrack_output_folder)
 
     return people_dict
